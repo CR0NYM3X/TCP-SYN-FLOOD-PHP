02032959e74ef02fe62354ada404d1990e5966d5
CR0NYM3X <alejandro.135t@gmail.com>
2021-08-10 01:37:24 -0700

    new

---
+++ send.php
@@ -0,0 +1,26 @@
+
+
+<?php
+
+
+/*
+ejemplos:
+
+https://www.redeszone.net/tutoriales/seguridad/hping3-manipular-paquetes-tcp-ip-ataques/
+
+Creacion paquetes tcp con socket RAW
+https://gist.github.com/fffaraz/57144833c6ef8bd9d453
+
+
+creacion de paquete
+https://inc0x0.com/tcp-ip-packets-introduction/tcp-ip-packets-4-creating-a-syn-port-scanner/
+
+
+
+sniffer python
+
+https://www.uv.mx/personal/angelperez/files/2018/10/sniffers_texto.pdf
+*/
+
+
+?>
